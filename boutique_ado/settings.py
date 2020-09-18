ea95eeb3c619c4eff17de67ced8520eefe514685
jamiebird119 <jamie.bird119@gmail.com>
2020-09-18 11:00:46 +0000

    set up stripe payment system to checkout

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -176,5 +176,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_CURRENCY = 'gbp'
+
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, reverse
 from .forms import OrderForm
 from django.contrib import messages
+from django.conf import settings
+from bag.context import bag_contents
+import stripe
 
 
 # Create your views here.
@@ -11,8 +14,24 @@ def checkout(request):
         return redirect(reverse('products'))
 
     order_form = OrderForm()
+    id_stripe_public_key = settings.STRIPE_PUBLIC_KEY
+
+    if not id_stripe_public_key:
+        messages.warning(request,
+                         "Stripe public key is missing.\
+                              Did you forget to add it?")
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,)
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'id_stripe_public_key': id_stripe_public_key,
+        'id_client_secret': intent.client_secret,
     }
     return render(request, template, context)
