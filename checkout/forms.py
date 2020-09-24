7c0842d41a386f23fcdbffd2558fb628b321f2f7
jamiebird119 <jamie.bird119@gmail.com>
2020-09-24 09:28:28 +0000

    Set up profiles app and added profile template

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,21 +20,22 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County',
         }
-
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        self.fields['country'].label = False
         for field in self.fields:
-            if field == 'full_name':
-                self.fields[field].widget.attrs['autofocus'] = True
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
+            if field != "country":
+                if field == 'full_name':
+                    self.fields[field].widget.attrs['autofocus'] = True
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False
