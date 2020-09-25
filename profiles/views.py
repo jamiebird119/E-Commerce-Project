8d23de6292c28d2497a85976cb97ad7e7f9a4e3d
jamiebird119 <jamie.bird119@gmail.com>
2020-09-25 10:11:29 +0000

    Set up profiles page with user information form and post method

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
+from django.contrib import messages
 
 
 # Create your views here.
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated delivery information.')
+
     template = 'profiles/profile.html'
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
     context = {
-        'profile': profile
+        'form': form,
+        'orders': orders,
+        'on_profiles_page': True,
     }
     return render(request, template, context)
+
